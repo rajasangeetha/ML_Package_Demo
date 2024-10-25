137787f37f0fe7f63e07ee5bef88d856d3e1b785
rajasangeetha <36691580+rajasangeetha@users.noreply.github.com>
2024-10-25 18:47:31 -0400

    Update __init__.py

--- ml_packages/__init__.py
+++ ml_packages/__init__.py
@@ -1,3 +1,3 @@
-from .build_model import buildLogisticRegressionModel
+from .build_model import buildLogisticRegressionModel, buildLinearRegressionModel
 from .data_preprocessing import drop_single_value_columns, check_and_drop_all_null_columns, drop_ID_columns, performImputation, lableEncoding, performDummyEncoding, performScale_Normalize
 from .file_processing import read_file
